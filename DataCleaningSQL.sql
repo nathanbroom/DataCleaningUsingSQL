cbe58e39208031ce6ade87b16793dcf762f4ddeb
Nathan Broom <49410994+nathanbroom@users.noreply.github.com>
2023-07-09 19:36:19 -0500

    Update DataCleaningSQL.sql

--- DataCleaningSQL.sql
+++ DataCleaningSQL.sql
@@ -1,7 +1,14 @@
 /*
+In this project, I cleaned raw data from the Nashville housing market to be used for further analysis. 
 
-Data Cleaning with SQL
-
+Cleaning Tasks
+1. Remove duplicated rows
+2. Populate PropertyAddress data where fields were null
+3. Breakout the PropertyAddress and OwnerAddress fields from a single column to multiple 
+   columns for each address component (address, city, state)
+4. Standardize the SoldAsVacant fields from 'Yes', 'No', 'Y', and 'N' to just 'N' and 'Y'
+5. Covert SoldDate fields from Datetime to Date
+6. Remove unused columns
 */
 
 /*########################################################################################################################
